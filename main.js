5c3a58ebc3d79f904ab45011ae6355a03bc03e23
MartMonster <martmonster1@gmail.com>
2022-02-12 18:38:20 +0100

    tried to do some stuff

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
+import { mapbox, stuff } from './APIKeys.json';
+console.log(mapbox);
+console.log(stuff);
+
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
