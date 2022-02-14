c41074f33293a658261c5e87ecaef6f1fef9b471
MartMonster <martmonster1@gmail.com>
2022-02-14 13:57:21 +0100

    train tracks get painted onto the map

--- front-end/main.js
+++ front-end/main.js
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     var map = L.map('map').setView([52.1009, 5.6463], 8);
 
+    $.getJSON("spoorkaart/spoorkaart.json", function(data) {
+        L.geoJSON(data.payload.features).addTo(map);
+    });
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>',
         maxZoom: 19,
@@ -27,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
         .openPopup();
 
     try {
-        Trains.getVehicles(53.2113, 6.5658, 1000);
+        //Trains.getVehicles(53.2113, 6.5658, 1000);
     } catch(e) {
         alert("query didn't come back OK:\n"+e);
     }
