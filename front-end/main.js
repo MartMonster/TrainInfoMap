0f4653ea4e88941c26dfff4f88115af33b4e2b04
MartMonster <martmonster1@gmail.com>
2022-02-18 22:20:28 +0100

    removes other stations when clicking on train with correct icons

--- front-end/main.js
+++ front-end/main.js
@@ -5,57 +5,148 @@ document.addEventListener("DOMContentLoaded", () => {
     //map initialization
     var map = L.map('map', {attributionControl: false}).setView([52.1009, 5.6463], 8);
 
-    $.getJSON("spoorkaart/spoorkaart.json", function(data) {
-        L.geoJSON(data.payload.features).addTo(map);
-    });
-
-    $.getJSON("stations/stations.json", function(data) {
-        console.log(data);
-        let stationIcon = L.icon({
-            iconUrl: "img/train-station.png",
-            shadowUrl: "img/train-station-shadow.png",
-
-            iconSize: [25,25],
-            shadowSize: [25,25],
-            iconAnchor: [12.5,25],
-            shadowAnchor: [12.5,25],
-            popupAnchor: [0,0]
-        });
-
-        data.payload.forEach(function(station) {
-            if ((station.stationType === "KNOOPPUNT_INTERCITY_STATION" || station.stationType === "MEGA_STATION") && station.land === "NL") {
-                L.marker([station.lat, station.lng], {icon: stationIcon}).addTo(map);
-            }
-        })
-    })
-
-    let trainIcon = L.icon({
-        iconUrl: "img/train-ns.png",
-        shadowUrl: "img/train-shadow.png",
-
-        iconSize: [30,30],
-        shadowSize: [30,30],
-        iconAnchor: [15,30],
-        shadowAnchor: [15,30],
-        popupAnchor: [0,-25]
-    });
-
-    let minorStationIcon = L.icon({
-        iconUrl: "img/minor-train-station.png",
-        shadowUrl: "img/minor-train-station.png",
-
-        iconSize: [15,15],
-        shadowSize: [15,15],
-        iconAnchor: [7.5,7.5],
-        shadowAnchor: [7.5,7.5],
-        popupAnchor: [0,0]
-    })
-
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>',
         maxZoom: 19,
         minZoom: 6,
     }).addTo(map);
+
+    $.getJSON("spoorkaart/spoorkaart.json", function(data) {
+        L.geoJSON(data.payload.features).addTo(map);
+    });
+
+    let trainIcon = L.icon({
+        iconUrl: "img/train-ns.png",
+        shadowUrl: "img/train-shadow.png",
+
+        iconSize: [30, 30],
+        shadowSize: [30, 30],
+        iconAnchor: [15, 30],
+        shadowAnchor: [15, 30],
+        popupAnchor: [0, -25]
+    });
+
+    let minorStation = L.icon({
+        iconUrl: "img/minor-train-station.png",
+        shadowUrl: "img/minor-train-station.png",
+
+        iconSize: [10, 10],
+        shadowSize: [10, 10],
+        iconAnchor: [5, 5],
+        shadowAnchor: [5, 5],
+        popupAnchor: [0, -10]
+    })
+
+    let knooppuntMinorStationIcon = L.icon({
+        iconUrl: "img/minor-train-station.png",
+        shadowUrl: "img/minor-train-station.png",
+
+        iconSize: [12.5, 12.5],
+        shadowSize: [12.5, 12.5],
+        iconAnchor: [6.25, 6.25],
+        shadowAnchor: [6.25, 6.25],
+        popupAnchor: [0,0]
+    })
+
+    let intercityStation = L.icon({
+        iconUrl: "img/minor-train-station.png",
+        shadowUrl: "img/minor-train-station.png",
+
+        iconSize: [15, 15],
+        shadowSize: [15, 15],
+        iconAnchor: [7.5, 7.5],
+        shadowAnchor: [7.5, 7.5],
+        popupAnchor: [0, 0]
+    })
+
+    let knooppuntICStation = L.icon({
+        iconUrl: "img/train-station.png",
+        shadowUrl: "img/train-station-shadow.png",
+
+        iconSize: [25, 25],
+        shadowSize: [25, 25],
+        iconAnchor: [12.5, 25],
+        shadowAnchor: [12.5, 25],
+        popupAnchor: [0, 0]
+    });
+
+    let megaStation = L.icon({
+        iconUrl: "img/train-station.png",
+        shadowUrl: "img/train-station-shadow.png",
+
+        iconSize: [30, 30],
+        shadowSize: [30, 30],
+        iconAnchor: [15, 30],
+        shadowAnchor: [15, 30],
+        popupAnchor: [0, 0]
+    });
+
+    
+    try {
+        Trains.getVehicles(53.2113, 6.5658, 1000).then(function (result) {
+            console.log(result);
+            result.payload.treinen.forEach(function (trein) {
+                L.marker([trein.lat, trein.lng], { title: `${trein.ritId}`/* , speed: trein.snelheid, direction: richting */, icon: trainIcon })
+                    .addTo(map).on('click', onClick)
+                    .bindPopup(`Trein ID: ${trein.ritId}`)
+                    .getPopup().on('remove', onClose);
+            })
+        });
+    } catch (e) {
+        alert("query didn't come back OK:\n" + e);
+    }
+
+    let allStationsRaw;
+    let majorStations = new Array();
+    $.getJSON("stations/stations.json", function (data) {
+        console.log(data);
+        allStationsRaw = data.payload;
+        data.payload.forEach(function (station) {
+            if ((station.stationType === "KNOOPPUNT_INTERCITY_STATION" || station.stationType === "MEGA_STATION") && station.land === "NL") {
+                //L.marker([station.lat, station.lng], { icon: knooppuntICStation }).addTo(map);
+                addStationToMap(station, null, majorStations);
+            }
+            
+        })
+    })
+
+    async function addStationToMap(station = null, stop = null, array = null) {
+        if (station !== null) {
+            var marker = null;
+            switch (station.stationType) {
+                case "MEGA_STATION": marker = L.marker([station.lat, station.lng], { icon: megaStation }).addTo(map); break;
+                case "KNOOPPUNT_INTERCITY_STATION": marker = L.marker([station.lat, station.lng], { icon: knooppuntICStation }).addTo(map); break;
+                case "INTERCITY_STATION": marker = L.marker([station.lat, station.lng], { icon: intercityStation }).addTo(map); break;
+                case "KNOOPPUNT_SNELTREIN_STATION": marker = L.marker([station.lat, station.lng], { icon: knooppuntMinorStationIcon }).addTo(map); break;
+                case "KNOOPPUNT_STOPTREIN_STATION": marker = L.marker([station.lat, station.lng], { icon: knooppuntMinorStationIcon }).addTo(map); break;
+                case "SNELTREIN_STATION": marker = L.marker([station.lat, station.lng], { icon: minorStation }).addTo(map); break;
+                case "STOPTREIN_STATION": marker = L.marker([station.lat, station.lng], { icon: minorStation }).addTo(map); break;
+                default: marker = L.marker([station.lat, station.lng]).addTo(map); console.log("something went wrong: " + station.stationType);
+            }
+            if (array != null) {
+                array.push(marker);
+            }
+        } else if (stop !== null) {
+            allStationsRaw.forEach(function(station) {
+                if(station.UICCode === stop.uicCode) {
+                    var marker = null;
+                    switch (station.stationType) {
+                        case "MEGA_STATION": marker = L.marker([station.lat, station.lng], { icon: megaStation }).addTo(map); break;
+                        case "KNOOPPUNT_INTERCITY_STATION": marker = L.marker([station.lat, station.lng], { icon: knooppuntICStation }).addTo(map); break;
+                        case "INTERCITY_STATION": marker = L.marker([station.lat, station.lng], { icon: intercityStation }).addTo(map); break;
+                        case "KNOOPPUNT_SNELTREIN_STATION": marker = L.marker([station.lat, station.lng], { icon: knooppuntMinorStationIcon }).addTo(map); break;
+                        case "KNOOPPUNT_STOPTREIN_STATION": marker = L.marker([station.lat, station.lng], { icon: knooppuntMinorStationIcon }).addTo(map); break;
+                        case "SNELTREIN_STATION": marker = L.marker([station.lat, station.lng], { icon: minorStation }).addTo(map); break;
+                        case "STOPTREIN_STATION": marker = L.marker([station.lat, station.lng], { icon: minorStation }).addTo(map); break;
+                        default: marker = L.marker([station.lat, station.lng]).addTo(map); console.log("something went wrong: " + station.stationType);
+                    }
+                    if (array != null) {
+                        array.push(marker);
+                    }
+                }
+            });
+        }
+    }
 
     if(!navigator.geolocation) {
         console.log("Your browser doesn't support geolocation")
@@ -67,13 +158,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let trainStops = new Array();
     function onClick() {
+        majorStations.forEach((station) => {
+            map.removeLayer(station);
+        })
         console.log(this.options.title);
         Trains.getStopsForTrain(this.options.title).then(function(result) {
             console.log(result);
             result.payload.stops.forEach(function(stop) {
-                console.log("type:" + stop.locationType);
                 if((stop.status === "ORIGIN" || stop.status === "STOP" || stop.status === "DESTINATION")) {
-                    trainStops.push(L.marker([stop.stop.lat, stop.stop.lng], {icon: minorStationIcon}).addTo(map));
+                    addStationToMap(null, stop.stop, trainStops);
                 }
             })
         });
@@ -81,9 +174,12 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function onClose() {
+        majorStations.forEach((station) => {
+            station.addTo(map);
+        });
         trainStops.forEach((stop) => {
             map.removeLayer(stop);
-        })
+        });
         trainStops = new Array();
     }
 
@@ -100,18 +196,4 @@ document.addEventListener("DOMContentLoaded", () => {
         .bindPopup('Een popup in het<br>midden van Nederland!')
         .openPopup();
     */
-
-    try {
-        Trains.getVehicles(53.2113, 6.5658, 1000).then(function(result) {
-            console.log(result);
-            result.payload.treinen.forEach(function(trein) {
-                L.marker([trein.lat, trein.lng], {title: `${trein.ritId}`/* , speed: trein.snelheid, direction: richting */, icon: trainIcon})
-                    .addTo(map).on('click', onClick)
-                    .bindPopup(`Trein ID: ${trein.ritId}`)
-                    .getPopup().on('remove', onClose);
-            })
-        });
-    } catch(e) {
-        alert("query didn't come back OK:\n"+e);
-    }
 });
